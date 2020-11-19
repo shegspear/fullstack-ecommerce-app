01f66074ba886b7d8654720a733c8d8865c97c46
shegspear <shegspear@gmail.com>
2020-11-19 16:01:14 +0100

    Implement React Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,26 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 
 import Header from './Components/Header';
 import Footer from './Components/Footer';
 
 import HomeScreen from './Screens/HomeScreen';
+import ProductScreen from './Screens/ProductScreen';
 
 
 const App =() => {
   return (
-    <>
-    <Header />
-    <main className="py-3">
-      <Container>
-        <HomeScreen />
-      </Container>
-    </main>
-    <Footer /> 
-    </>
+    <Router>
+     <Header />
+       <main className="py-3">
+        <Container>
+          <Route path='/' component={HomeScreen} exact />
+          <Route path='/product/:id' component={ProductScreen}  />
+        </Container>
+        </main>
+      <Footer /> 
+    </Router>
   );
 }
 
---
+++ frontend/src/Screens/ProductScreen.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProductScreen = () => {
+    return (
+        <div>
+            Poduct
+        </div>
+    )
+}
+
+export default ProductScreen

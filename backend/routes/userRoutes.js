056f8eed0e7e72684e4bc1995d1fc2a4aba990eb
shegspear <shegspear@gmail.com>
2021-10-03 22:20:15 +0100

    setup user registration and password encrytion in backend

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,12 +3,15 @@ const router = express.Router();
 
 import {
   authUser,
-  getUserProfile
+  registerUser,
+  getUserProfile,
 } from '../controllers/userController.js';
 
 import {
   protect,
 } from '../middleware/authMiddleware.js';
+
+router.route('/').post(registerUser);
 
 router.post('/login', authUser);
 

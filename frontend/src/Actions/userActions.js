ad1204f7627fbe79c4f1f73187aa9aa32fc1f4fc
shegspear <shegspear@gmail.com>
2021-10-04 23:15:14 +0100

    worked on showing login user in navbar and also logout feature

--- frontend/src/Actions/userActions.js
+++ frontend/src/Actions/userActions.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
 import { 
-    USER_LOGIN_FAIL, 
-    USER_LOGIN_REQUEST, 
-    USER_LOGIN_SUCCESS 
+  USER_LOGIN_FAIL, 
+  USER_LOGIN_REQUEST, 
+  USER_LOGIN_SUCCESS, 
+  USER_LOGOUT,
 } from "../Constants/userConstants";
 
 export const login = (email, password) => async (dispatch) => {
@@ -40,4 +41,9 @@ export const login = (email, password) => async (dispatch) => {
     });
   }
 
+};
+
+export const logout = () => (dispatch) => {
+  localStorage.removeItem('userInfo');
+  dispatch({ type: USER_LOGOUT}) 
 };

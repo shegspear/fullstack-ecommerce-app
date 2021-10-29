2f5005ce638cffcfc281b795ebe42ae4d92da773
shegspear <shegspear@gmail.com>
2021-10-29 13:26:42 +0100

    setup the admin get user and update user endpoints

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -8,6 +8,8 @@ import {
   updateUserProfile,
   getUsers,
   deleteUser,
+  getUserById,
+  updateUser,
 } from '../controllers/userController.js';
 
 import {
@@ -24,6 +26,11 @@ router
   .get(protect, getUserProfile)
   .put(protect, updateUserProfile);
 
-router.route('/:id').delete(protect, admin, deleteUser);
+router
+  .route('/:id')
+  .delete(protect, admin, deleteUser)
+  .get(protect, admin, getUserById)
+  .put(protect, admin, updateUser);
+                    
 
 export default router;
